ef78dca0740bc1a598863dfac0cc60cf437269f3
ikoamu <ikoamu@gmail.com>
2022-05-03 11:53:23 +0000

    amplify auth updateした

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,6 +16,9 @@ export type AmplifyDependentResourcesAttributes = {
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "pokerGroupRole": "string"
         }
     }
 }
