08531a308ab7c90dbb56828d7f39d1dc44ad71bf
ikoamu <ikoamu@gmail.com>
2022-05-05 20:30:17 +0000

    amplify studioからコンポーネントをimportしてみた

---
+++ src/ui-components/Input.jsx
@@ -0,0 +1,46 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Flex, Text } from "@aws-amplify/ui-react";
+export default function Input(props) {
+  const { overrides: overridesProp, ...rest } = props;
+  const overrides = { ...overridesProp };
+  return (
+    <Flex
+      gap="10px"
+      direction="row"
+      alignItems="flex-start"
+      position="relative"
+      border="1px SOLID rgba(189,189,189,1)"
+      borderRadius="8px"
+      padding="9px 9px 9px 9px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "Flex")}
+    >
+      <Text
+        fontFamily="Inter"
+        fontSize="12px"
+        fontWeight="400"
+        color="rgba(189,189,189,1)"
+        lineHeight="14.0625px"
+        textAlign="left"
+        display="flex"
+        direction="column"
+        justifyContent="flex-start"
+        shrink="0"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="プレースホルダ"
+        {...getOverrideProps(overrides, "Flex.Text[0]")}
+      ></Text>
+    </Flex>
+  );
+}
