92808903e422f09df278d68c15bbbc6ad939c19c
ikoamu <ikoamu@gmail.com>
2022-06-11 09:04:43 +0000

    リダイレクトURLを環境変数にした

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,25 +3,21 @@ import type { AppProps } from 'next/app';
 import Amplify from 'aws-amplify';
 import awsconfig from '../src/aws-exports';
 
-const isLocalhost = process.env.NODE_ENV === 'development';
-
-// Assuming you have two redirect URIs, and the first is for localhost and second is for production
-const [localRedirectSignIn, productionRedirectSignIn] =
-  awsconfig.oauth.redirectSignIn.split(',');
-
-const [localRedirectSignOut, productionRedirectSignOut] =
-  awsconfig.oauth.redirectSignOut.split(',');
+console.log(
+  'process.env.REDIRECT_SIGN_IN_URL',
+  process.env.REDIRECT_SIGN_IN_URL
+);
+console.log(
+  'process.env.REDIRECT_SIGN_OUT_URL',
+  process.env.REDIRECT_SIGN_OUT_URL
+);
 
 const updatedAwsConfig = {
   ...awsconfig,
   oauth: {
     ...awsconfig.oauth,
-    redirectSignIn: isLocalhost
-      ? localRedirectSignIn
-      : productionRedirectSignIn,
-    redirectSignOut: isLocalhost
-      ? localRedirectSignOut
-      : productionRedirectSignOut,
+    redirectSignIn: process.env.REDIRECT_SIGN_IN_URL,
+    redirectSignOut: process.env.REDIRECT_SIGN_OUT_URL,
   },
   ssr: true,
 };
