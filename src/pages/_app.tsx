c16389fe72e54558f8f080456036c48ff38854c3
ikoamu <ikoamu@gmail.com>
2022-08-05 13:30:40 +0000

    faviconを設定した

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { Amplify } from 'aws-amplify';
 import awsconfig from '../aws-exports';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Head from 'next/head';
 
 const updatedAwsConfig = {
   ...awsconfig,
@@ -37,6 +38,18 @@ const theme = createTheme({
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>planning-poker</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🃏</text></svg>"
+        />
+        <link
+          rel="icon alternate"
+          type="image/png"
+          href="https://twemoji.maxcdn.com/v/14.0.2/72x72/1f0cf.png"
+        />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
