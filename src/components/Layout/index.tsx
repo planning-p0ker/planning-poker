7749a8eba4d5acbc4b68002f39e8f83ab677a35c
ikoamu <ikoamu@gmail.com>
2025-11-08 20:56:25 +0900

    fix: update link to new planning poker app and remove GitHub link

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -26,15 +26,14 @@ export const Layout: React.FC<PropsWithChildren<LayoutProps>> = ({
       />
       <Divider />
       <main className="max-w-3xl mx-auto pt-10">{children}</main>
-      {/* <Link
+      <Link
         sx={{ marginTop: 20 }}
         className="flex space-x-1 justify-center"
-        href="https://github.com/planning-p0ker/planning-poker"
+        href="https://poker.ikoamu.app/"
         target="_blank"
       >
-        <GitHubIcon style={{ color: 'black' }} />
-        <div>https://github.com/planning-p0ker/planning-poker</div>
-      </Link> */}
+        🦆 New planning poker app is out! 🦆
+      </Link>
     </>
   );
 };
